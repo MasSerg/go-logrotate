1eddb3aae72996ccc1d91c10602ffbbc56000689
MasSerg <masserg2007@gmail.com>
2019-10-02 13:53:36 +0300

    initial

---
+++ README.md
@@ -0,0 +1,69 @@
+# Log File Archiving Script by CRON
+  
+1. Setup is performed by writing a list of the necessary files for archiving in the “.env” configuration file.  
+  
+    Example:  
+        ROOT_COMPRESSED_LOG_DIRECTORY = "/path/to/log/arch/dir" - archive files save location  
+        LOG_FILES = "/path/to/logfile/location = file_name1.log; /path/to/logfile/location = file_name2 .log" - path to file and name of the log file  
+  
+2. Files from the list will be renamed with the prefix of the date and time of renaming (YYYY_MM_DD_HHIISS_file_name.log); zip compressed; archive files will be moved to the specified directory, files with a prefix will be deleted and new log files with permissions 0666 will be created.  
+  
+  
+# Install GO
+
+```bash
+apt install snapd  
+```
+Restart your  session  
+```bash  
+snap install go --classic  
+```
+```bash
+go version  
+```
+  
+  
+# Deploy  
+
+```bash
+git clone ...
+```   
+```bash
+echo "GOPATH=$HOME/.go-logrotate" >> ~/.bashrc
+```
+```bash  
+cd ~/go-logrotate
+```
+```bash  
+go get -d ./
+```  
+  
+  
+# Build & run  
+```bash
+go build  
+```
+```bash
+./go-logrotate
+```  
+  
+# Add CRON job  
+
+```bash
+crontab -e
+```  
+```bash 
+# Example of job definition:  
+# .---------------- minute (0 - 59)  
+# |  .------------- hour (0 - 23)  
+# |  |  .---------- day of month (1 - 31)  
+# |  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ...  
+# |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat  
+# |  |  |  |  |  
+# *  *  *  *  * user-name  command to be executed
+```  
+```bash   
+crontab -l
+```  
+
+
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"github.com/joho/godotenv"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get .env variables
+	logDirectory := os.Getenv("ROOT_COMPRESSED_LOG_DIRECTORY")
+	strLogFiles := os.Getenv("LOG_FILES")
+
+	// Get prefix file name with date for zip file
+	t := time.Now()
+	dateFilePrefix := fmt.Sprintf("%d_%02d_%02d_%02d%02d%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+
+	if _, err := os.Stat(logDirectory); os.IsNotExist(err) {
+		os.Mkdir(logDirectory, 0666)
+	}
+
+	items := strings.Split(strLogFiles, "; ")
+	for _, e := range items {
+		parts := strings.Split(e, " = ")
+		logFilePath := parts[0]
+		logFileName := parts[1]
+		newFileName := dateFilePrefix+"_"+logFileName
+
+		// Rename original file for compressing
+		if err := os.Rename(logFilePath+"/"+logFileName, logDirectory+"/"+newFileName+".log"); err != nil {
+			log.Print(err.Error())
+		}
+
+		// Create new file
+		f, e := os.Create(logFilePath+"/"+logFileName)
+		if e != nil {
+			log.Print(err.Error())
+		}
+		defer f.Close()
+
+		if err := os.Chmod(logFilePath+"/"+logFileName, 0666); err != nil {
+			log.Println(err)
+		}
+
+		// Compress file to zip
+		newZipFile, err := os.Create(logDirectory+"/"+newFileName+".zip")
+		if err != nil {
+			log.Print(err.Error())
+		}
+		defer newZipFile.Close()
+
+		zipWriter := zip.NewWriter(newZipFile)
+		defer zipWriter.Close()
+
+		if err = AddFileToZip(zipWriter, logDirectory, newFileName); err != nil {
+			log.Print(err.Error())
+		}
+
+		// Remove log file after compressing
+		if err := os.Remove(logDirectory+"/"+newFileName+".log"); err != nil {
+			log.Print(err.Error())
+		}
+	}
+
+}
+
+func AddFileToZip(zipWriter *zip.Writer, directory string, filename string) error {
+
+	fileToZip, err := os.Open(directory+"/"+filename+".log")
+	if err != nil {
+		return err
+	}
+	defer fileToZip.Close()
+
+	// Get the file information
+	info, err := fileToZip.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+
+	// Using FileInfoHeader() above only uses the basename of the file. If we want
+	// to preserve the folder structure we can overwrite this with the full path.
+	header.Name = filename
+
+	// Change to deflate to gain better compression
+	// see http://golang.org/pkg/archive/zip/#pkg-constants
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(writer, fileToZip)
+	return err
+}
